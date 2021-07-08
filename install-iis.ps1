9693db175308f3f78d4bb50e8d68c2f00f8a23c4
Tiago Costa <tiago.costa@outlook.com>
2021-07-08 20:18:22 +0100

    fixing ps

--- install-iis.ps1
+++ install-iis.ps1
@@ -1,3 +1,3 @@
 powershell.exe Install-WindowsFeature -name Web-Server -IncludeManagementTools
 powershell.exe Remove-Item -Path 'C:\inetpub\wwwroot\iisstart.htm'
-powershell.exe Add-Content -Path "C:\inetpub\wwwroot\iisstart.htm" -Value $("Hello World from " + $env:computername)
+powershell.exe Add-Content -Path 'C:\inetpub\wwwroot\iisstart.htm' -Value $("Hello World from " + $env:computername)
